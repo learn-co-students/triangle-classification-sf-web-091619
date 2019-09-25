d104ffbade2bb69bad1449a53214d2bb384ce07f
github username <github email address>
2019-09-25 16:27:29 -0700

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,38 @@
 class Triangle
   # write code here
+  
+  attr_accessor :a, :b, :c
+
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def valid 
+    (a > 0 && b > 0 && c > 0) && (a + b > c && a + c > b && b + c > a)
+  end 
+
+  def kind
+    if valid
+      true 
+    else 
+      raise TriangleError 
+    end 
+    if a == b && b == c && a == c
+      :equilateral
+    elsif a != b && b != c && a != c
+      :scalene 
+    else 
+      :isosceles
+    end 
+  end
+
+
+  class TriangleError < StandardError
+    # triangle error code
+    "You're a triangle."
+  end
+
+
 end
